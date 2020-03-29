215f4995f69ec0cde38effd2cddd580aa932e989
Anuj <apatel922@gmail.com>
2020-03-29 18:42:58 -0400

    Intern.js SUCCESS

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,8 +1,29 @@
 
-// school
+const Employee = require("../lib/Employee");
+
+class Intern extends Employee {
+    // school
+    constructor(name, id, email, school) {  
+      super(name, id, email);
+      this.school = school;
+    }
+}
 
 // getSchool()
+Intern.prototype.getSchool = function () {
+    return this.school;
+}
 
 // getRole() // Overridden to return 'Intern'
+Intern.prototype.getRole = function () {
+    return "Intern";
+}
 
-// module.exports = Intern;
+
+var newIntern = new Intern("Gimli", 1, "test@test.com", "GitHubUser");
+
+console.log(newIntern);
+newIntern.getSchool();
+newIntern.getRole();
+
+module.exports = Intern;
